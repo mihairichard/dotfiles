7e7146c1ba50a044dc21972b29dee50cb441d659
Mihai Richard <mihairichard@live.com>
2019-09-15 16:42:31 +0200

    Added zsh config

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+# vundle install
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo 'run :PluginInstall'
+
+# powerline fonts
+sudo apt-get install fonts-powerline
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
