8a5fc8c398051fcac43134e0d0c3769663c3a1bb
Mihai Richard <mihairichard@live.com>
2019-09-16 21:41:06 +0200

    Added script to enable tap to click in i3

---
+++ tap_to_click.sh
@@ -0,0 +1,11 @@
+# Enable tap to click on mb trackpad
+sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+Section "InputClass"
+        Identifier "touchpad"
+        MatchIsTouchpad "on"
+        Driver "libinput"
+        Option "Tapping" "on"
+EndSection
+
+EOF
+
